fb403203d2555229ac83cdd7904de82a19922fcf
Cynthia E <cynthia8499@hotmail.com>
2020-07-08 18:57:08 -0400

    half solution

--- actions.js
+++ actions.js
@@ -1,7 +1,23 @@
-const axios = require('axios');
+const axios = require("axios");
 
 //TYPES
-const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require('./types');
+const { GOT_DESSERTS_DATA, NEW_DESSERT_DATA } = require("./types");
+
+const gotDesserts = (desserts) => {
+  return {
+    type: GOT_DESSERTS_DATA,
+    desserts,
+  };
+};
+
+const addDessert = (newDessert) => {
+  return {
+    type: NEW_DESSERT_DATA,
+    newDessert,
+  };
+};
+const fetchDesserts = () => {};
+const postDessert = () => {};
 
 /* YOUR CODE GOES HERE */
 module.exports = { gotDesserts, addDessert, fetchDesserts, postDessert };
